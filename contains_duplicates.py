642d0c617770ec355a3de5917792627212e5c846
fadyfaddoul <fady_faddoul@hotmail.com>
2023-03-19 15:48:02 -0400

    contains_duplicates

---
+++ contains_duplicates.py
@@ -0,0 +1,15 @@
+def contains_duplicates(nums):
+    lookup=dict()
+
+    for entry in nums:
+        if (entry in lookup.keys()):
+            return True
+        else:
+            lookup.update({entry:1})
+    return False
+
+
+        
+print(contains_duplicates([1,1,1,3,3,4,3,2,4,2])) #True
+print(contains_duplicates([])) #False
+print(contains_duplicates([1,2,3,4,5])) #False
