1c0bd26b7ca044ceeadc193fef62df725de0d92e
fadyfadd <fady_faddoul@hotmail.com>
2025-09-17 17:29:26 +0300

    is_perfect_square

--- is_perfect_square.py
+++ is_perfect_square.py
@@ -1,9 +1,14 @@
 def is_perfect_square(num) -> bool:        
-        value=1
-        while (value*value < num):            
-            value = value+1
-        print (value*value)
-        return value*value==num
+        
+    if num < 0:
+        return False  
+    if num <= 1:       
+        return True 
+    value=1
+    while (value*value < num):            
+        value = value+1
+    print (value*value)
+    return value*value==num
 
 print(is_perfect_square(16))
     
